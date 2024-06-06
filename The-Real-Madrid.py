1261a26f8a33f39deeecc4c88d28fd6a8bc28650
2100090063 <113589379+2100090063@users.noreply.github.com>
2024-06-06 06:29:23 +0530

    Create The-Real-Madrid.py

---
+++ The-Real-Madrid.py
@@ -0,0 +1,12 @@
+# You are using Python
+n=int(input())
+l=list(map(int,input().split()))
+for i in l:
+    if l.count(i)==2:
+        x=i
+        break
+sum1=n*(n+1)//2
+num=sum(l)
+num-=x
+num=sum1-num
+print(f"The missing jersey number is {num} and the duplicate number is {x}")
